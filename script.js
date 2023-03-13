09a44e229a0d5606b19a1059107a465f57314eb2
Nhan Duong <nhanduong@mjreseattle.com>
2023-03-12 20:50:18 -0700

    update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ startButton.addEventListener("click", function () {
     startButton.classList.add("is-hidden")
     //create function that starts a timer countdown, and call it inside here
     startTimer()
-
+    showQuestion(0)
 })
 var timerInterval;
 var countdownTimer = 10
@@ -34,32 +34,24 @@ function startTimer() {
 
 }
 
-//global variables
-//var questions = ["what is 2 + 2", "fsdfsdf"]
-// var answers = [["2,3,4,5"], ["china", "costa rica," "canada", "chile"]]
-// var correct = [2, 0]
-// var timeLeft = 120
-// var currentQuestion = 0
+var question = [
+    {
+        text: "1+1",
+        choices: ["1","2","3","4"],
+        correct:"2"
+    },
+    {
+        text: "1+2",
+        choices: ["1","2","3","4"],
+        correct:"3"
+    }
+]
 
-//function startGame()
-// initialize first question
-// start timer
-
-// function checkAnswer()
-// called when answer selected
-//checkes if right or wrong
-//deducts from timer if wrong
-// maybe indicate to user if wrong
-// call nextQuestion
-
-
-// function next Question
-// loads next question
-
-// function endGame()
-// triggered by last question being answeres or timer hit zero
-// prompts for initials
-// saves high score
-//shows high scores
-// play again button
-
+const showQuestion = (ind)=>{
+    questionTextEl.textContent=question[ind].text;
+    question[ind].choices.forEach(function (ans){
+        var btn = document.createElement("button")
+        btn.textContent=ans
+        questionChoicesEl.appendChild(btn)
+    })
+}
